89130500abc87552c6b3f010d16555ef15bc194e
Angela J Holden <angelajholden@gmail.com>
2026-05-10 12:29:33 -0500

    form styles and validation

---
+++ modules/form-validation.js
@@ -0,0 +1,48 @@
+export default function initFormValidation() {
+	const root = document.querySelector(".form");
+	if (!root) return;
+
+	const origin = root.querySelector('input[name="origin"]');
+	const path = root.querySelector('input[name="pathname"]');
+
+	origin.value = window.location.origin;
+	path.value = window.location.pathname;
+
+	const name = root.querySelector('input[name="name"]');
+	const email = root.querySelector("input[name=email]");
+
+	const nameError = root.querySelector("#name-required");
+	const emailError = root.querySelector("#email-required");
+
+	root.addEventListener("submit", (e) => {
+		if (name.value === null || name.value === "") {
+			e.preventDefault();
+			nameError.hidden = false;
+			return;
+		} else {
+			nameError.hidden = true;
+		}
+
+		if (email.value === null || email.value === "") {
+			e.preventDefault();
+			emailError.hidden = false;
+			return;
+		} else {
+			emailError.hidden = true;
+		}
+	});
+
+	name.addEventListener("input", () => {
+		nameError.hidden = true;
+	});
+
+	email.addEventListener("input", () => {
+		emailError.hidden = true;
+	});
+
+	const reset = root.querySelector(".reset_button");
+	reset.addEventListener("click", () => {
+		nameError.hidden = true;
+		emailError.hidden = true;
+	});
+}
--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 import initNavSearchToggle from "./modules/navigation.js";
 import initCopyright from "./modules/copyright.js";
+import initFormValidation from "./modules/form-validation.js";
 // import initTheSlider from "./modules/slider.js";
 
 initNavSearchToggle();
 initCopyright();
+initFormValidation();
 // initTheSlider();
